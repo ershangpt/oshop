043a1ff5435fb7bd6d0511107f339f9f5ac7d373
Shasha <ershangpt@gmail.com>
2018-11-23 23:39:51 +0530

    first commit 1

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { Subscription } from 'rxjs';
+import { Product } from '../../models/products';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts;
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = productService.getAll();    
+    this.subscription = productService.getAll().subscribe(product => this.filteredProducts = this.products = product);    
    }
 
+   filter(query: string) {
+     this.filteredProducts = (query) ? 
+            this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+            this.products;
+   }
+
+   ngOnDestroy() {
+    this.subscription.unsubscribe();
+   }
   ngOnInit() {
   }
 
